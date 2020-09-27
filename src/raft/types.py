94c4eef571a5667468307510e832f469d52e541c
alexjperkins <alexjperkins@hotmail.co.uk>
2020-09-27 21:54:55 +0000

    remove: types

--- src/raft/types.py
+++
@@ -1,9 +0,0 @@
-from typing import NewType
-
-from . network.messages import Address, NetworkMessage
-from . sockets.socket import Socket
-
-
-AddressType = NewType("AddressType", Address)
-NetworkMessageType = NewType("NetworkMessageType", NetworkMessage)
-SocketType = NewType("SocketType", Socket)
